608bdb6ecafda1809455231e53cdd4a9465b6cc1
LoBrol <andreabrollo047@gmail.com>
2024-01-10 20:01:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install -y python3 python3-pip
+apt install -y wget python3 python3-pip
 pip install jupyterlab
+mkdir /root/jupyter-notebooks
+sh -c "$(wget https://raw.githubusercontent.com/LoBrol/ubuntu_docker/main/file_to_be_copied/install-jupyter-service.sh -O -)"
